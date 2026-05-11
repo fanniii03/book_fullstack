56fc18b7af633bafd3ead83c0047fdcd3d05eaf0
_fancsi03 <balomfanni2003@gmail.com>
2026-05-11 21:39:53 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+﻿const API_BASE_URL = 'http://localhost:5056/api/books'
+let books = []
+
+async function downloadAndDisplay() {
+}
+
+function loadForUpdate(event) {
+}
+
+async function deleteBook(event) {
+}
+
+function resetForm() {
+  
+}
+
+function addTemp() {
+
+}
+
+async function createBook() {
+
+}
+
+async function updateBook() {
+
+}
+
+// TODO: Hívd meg induláskor a downloadAndDisplay() függvényt.
